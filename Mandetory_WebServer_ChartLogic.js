76434f987c3cc00c1213d27f3daae3bcf60958bb
girish1052 <84569384+girish1052@users.noreply.github.com>
2021-05-21 16:24:46 +0530

    Update Mandetory_WebServer_ChartLogic.js

--- Mandetory_WebServer_ChartLogic.js
+++ Mandetory_WebServer_ChartLogic.js
@@ -9,6 +9,8 @@ var getScriptPromisify = (src) => {
     //Chart Block in HTML
     const prepared = document.createElement('template')
     prepared.innerHTML = `
+        <style>
+        </style>
         <div id="root" style="width: 100%; height: 100%;">
         </div>
       `
@@ -43,15 +45,13 @@ var getScriptPromisify = (src) => {
       
       //render() method to plot chart - resultSet1 holds data from SAC table/chart.
       async render (resultSet1) {
-        await getScriptPromisify('https://code.highcharts.com/highcharts.js')
+        await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
 
 
         const region = []
         const sales =[]
         const quntity = []
         var j = 0
-
-        console.log(resultSet1)
 
         var len = resultSet1.length
 
@@ -68,78 +68,82 @@ var getScriptPromisify = (src) => {
             j++
           }
         }
-
-        Highcharts.chart('container', {
-
-          title: {
-              text: 'Solar Employment Growth by Sector, 2010-2016'
-          },
-      
-          subtitle: {
-              text: 'Source: thesolarfoundation.com'
-          },
-      
-          yAxis: {
-              title: {
-                  text: 'Number of Employees'
-              }
-          },
-      
-          xAxis: {
-              accessibility: {
-                  rangeDescription: 'Range: 2010 to 2017'
-              }
-          },
-      
-          legend: {
-              layout: 'vertical',
-              align: 'right',
-              verticalAlign: 'middle'
-          },
-      
-          plotOptions: {
-              series: {
-                  label: {
-                      connectorAllowed: false
-                  },
-                  pointStart: 2010
-              }
-          },
-      
-          series: [{
-              name: 'Installation',
-              data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-          }, {
-              name: 'Manufacturing',
-              data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-          }, {
-              name: 'Sales & Distribution',
-              data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-          }, {
-              name: 'Project Development',
-              data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-          }, {
-              name: 'Other',
-              data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-          }],
-      
-          responsive: {
-              rules: [{
-                  condition: {
-                      maxWidth: 500
-                  },
-                  chartOptions: {
-                      legend: {
-                          layout: 'horizontal',
-                          align: 'center',
-                          verticalAlign: 'bottom'
-                      }
-                  }
-              }]
-          }
-      
-      });
   
+        const chart = echarts.init(this._root)
+
+        const option = {
+        
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // Use axis to trigger tooltip
+              type: 'shadow'        // 'shadow' as default; can also be 'line' or 'shadow'
+            }
+          },
+        
+          legend: {
+            data: ['Sales', 'Quntity']  //At the top it will show legend
+          },
+        
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+            
+          xAxis: {
+				    type: 'value',
+			    },
+			
+          yAxis: {
+				    type: 'category',
+            data: region
+			    },
+			
+          series: [
+            {
+              name: 'Sales',
+              data: sales,
+              type: 'bar',
+              stack : 'total',
+              
+              showBackground: true,label: {
+                show: true
+              },
+
+              emphasis: {
+                focus: 'series'
+              },
+            
+              backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)'
+              }
+		    	  },
+            
+            {
+              name: 'Quntity',
+              data: quntity,
+              type: 'bar',
+              stack : 'total',
+              
+              showBackground: true,label: {
+                show: true
+              },
+            
+              emphasis: {
+                focus: 'series'
+              },
+              
+              backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)'
+              }
+			      }
+          ],
+            
+          animationDuration: 2000
+        
+        }
+        chart.setOption(option)
       }
     }
   
